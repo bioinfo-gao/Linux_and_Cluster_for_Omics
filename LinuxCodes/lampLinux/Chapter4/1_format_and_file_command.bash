5f835c6431e0af8c57e7e6c43548ce405090dfba
WSL_Oracle <bioinfo.gao@gmail.com>
2025-11-16 23:47:25 -0500

    file

--- LinuxCodes/lampLinux/Chapter4/1_format_and_file_command.bash
+++ LinuxCodes/lampLinux/Chapter4/1_format_and_file_command.bash
@@ -1,7 +1,6 @@
 #! /home/gao/bash 
 # cmd [-option] [parameters]
 ls -la /etc 
-
 # option can be used together -al == -al = -l -a
 # option has simplified and full format
 -a == --all
@@ -13,22 +12,64 @@ ld # ls -ld */  -d 让 ls 本身列出目录条目（而不是目录内容） */
 # -rwxr--rw-
 # u g o
 
-# mkdir recursively and touch a file 
-# mkdir -p ./tmp/tmp3_ZG/3rd_level_dir/f  && touch ./tmp/tmp3_ZG/3rd_level_dir/file.txt 
-# cp -r tmp/tmp3_ZG/    tmp/tmp1_ZG/
+mkdir ./tmp/
+mkdir ./tmp/Japan/boduo
+mkdir -r./tmp/Japan/boduo # mkdir recursively and touch a file 
+mkdir ./tmp/Japan/longze  ./tmp/Japan/cangjing
 
+mkdir -p ./tmp/tmp3_ZG/3rd_level_dir/f  && touch ./tmp/tmp3_ZG/3rd_level_dir/file.txt 
+# cp -r tmp/tmp3_ZG/    tmp/tmp1_ZG/     # recursive 
 llh ./tmp/tmp3_ZG/
 ll ./tmp/tmp3_ZG/3rd_level_dir/file.txt
 
-#cp -p tmp/tmp1_ZG/3rd_level_dir/    tmp/tmp1_ZG/3rd_level_dir_copy/ # keep file time etc
+cd 
+cd ..
+cd ../..
+cd /tmp/tmp3_ZG/
+cd -
+
+pwd
+
 
 rmdir tmp # delete blank dir
 rmdir tmp1
 
-rm -r tmp/tmp1_ZG
+rm -r tmp/tmp1_ZG                          # -r  recursive 
 
-cd ..
-cd
+mkdir  tmp/tmp1_ZG/3rd_level_dir_1/   
+cp  -r  tmp/tmp1_ZG/3rd_level_dir/    tmp/tmp/3rd_level_dir_renamed # copy and rename
+# -r keep file time etc
+# -p keep timestamp
+
+clear # Ctrl + l
+
+# move and rename 
+mv  tmp/tmp/3rd_level_dir_renamed   tmp/tmp1_ZG
+
+cp -r tmp/tmp1_ZG     tmp/tmp2_ZG    
+rm -rf tmp/tmp2_ZG    
+# -f
+# -r  
+
+touch ./tmp/story.list
+cd ./tmp
+touch shencao girl
+touch "shencao girl" # dot NOT use Space , troublesome 
+
+
+cat /etc/issue
+cat -n /etc/issue
+
+cat /etc/services
+tac /etc/services # reverse
+
+more /etc/services # page arrow down  or space  a page , enter a line 
+less /etc/services # page arrow down  or space 
+# /keyword  search , n   next-match
+
+head /etc/services # -n : n line 
+tail -n 3 /etc/services #  
+
 
 ln -s /home/gao/tmp/services service_link
 ln /home/gao/tmp/services service_hard
@@ -36,7 +77,7 @@ ls -i /home/gao/tmp/services
 ls -i  service_hard
 
 
-#sudo apt install plocate
+# sudo apt install locate # debian 
 locate service
 
 find /etc/ -name init
