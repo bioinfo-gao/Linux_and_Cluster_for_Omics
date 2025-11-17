3a088d19115d9b8800fcd5ba0ddbc0e27b2c5bb1
WSL_Oracle <bioinfo.gao@gmail.com>
2025-11-17 14:24:43 -0500

    find

--- LinuxCodes/lampLinux/Chapter4/3_file_search.sh
+++ LinuxCodes/lampLinux/Chapter4/3_file_search.sh
@@ -1,23 +1,58 @@
 # sudo apt install locate # debian 
 locate service
 
+# find file search, 默认精准搜索
 find /etc/ -name init
-find /etc -iname init
-find /etc -name *init*
-find /etc -name *init* -a -type f -exec ls -l {} \; # must keep the space "{} \;""
-find /etc -name *init* -a -type f -ok ls -l {}\;
-find /etc -name init??
-find /etc -name inittab 
-find /etc -user shenchao -ok rm  {} \; ##
-find . -inum 19275595   -exec rm  {} \; ## no space
+sudo find /etc/ -name init
+sudo find /etc -name init* # init 开头
+sudo find /etc -name init??? # init + 3 characters
+sudo find /etc -name inittab 
+sudo find /etc -iname init* 
+
+# +n, -n n 大于小于等于, 不能用如下搜索，因为wsl 能够搜索window11的硬盘，出现很多不应该出现的文件
+sudo find / -type f -size +5G -exec ls -lh {} \; 2>/dev/null
+# check how many Byte for 1 block 
+sudo blockdev --getbsz /dev/sda 
+# 4096  1 block = 4KB 
+# -2048 2048   这里的数字是block， 需要把block 换算成 大写 
+# 1M = 1024KB
+# 100M = 102400KB = 25600 block
+echo $((100 * 1024 * 1024 / 4096))  # 100M 文件换算成数据快
+
+sudo find /home/gao -type f -size +100M -exec ls -lh {} \; 2>/dev/null
+
+sudo find /etc -name *init* -a -type f -exec ls -l {} \; # must keep the space "{} \;""
+sudo find /etc -name *init* -a -type f -ok ls -l {}\;
+
+# 查找shencao的所有文件 -user 
+sudo find /home -user shencao #
+sudo find /home -group meinv #
+
+sudo find /home -user shencao -ok rm  {} \; ##
+sudo find . -inum 19275595   -exec rm  {} \; ## 删除顽固文件
+sudo find . -inum 19275595   -exec ls -l  {} \; ## 展示详细信息
+# -ok -exec
+
+# 注意原始方法是找block数目，而且恒定在512Byte， 不是本机4096Byte就改成4096， 所以会找出来 15M的文件
+sudo find /home/gao -type f -size +25600 -exec ls -lh {} \; 2>/dev/null # 查找大于 $12.5\text{ MB}$ 的文件
+sudo find / -type f -size +5G -exec ls -lh {} \; 2>/dev/null # 单位有c K M G 
+
+
 pwd
 
 
-find /etc -size +10f # 100 block, 4096 in this linux 
-find /etc -size +100  -a -size -2000 # larger than 100 block and less 2000 block  -o or
+find /etc -size +100 # 100 block, 4096 in this linux 
+sudo find /etc -size +100  -a -size -2000 # larger than 100 block and less 2000 block
+#  -o or 
+
 find /etc -user gao # 100 block, 4096 in this linux 
-find /etc -cmin 5 # amin access time, cmin chanes 文件属性, mmin modification 
-find /etc -cmin 5 # amin access time, cmin chanes 文件属性, mmin modification 
+
+help find
+man find
+# -amin access time, 
+# -cmin file status-e.g. owner 文件属性
+# -mmin content modification 
+find /etc -cmin -5   # less than 5min 
 
 sudo tune2fs -l /dev/sda1 | grep -i 'block size'
 #Block size:               1024
