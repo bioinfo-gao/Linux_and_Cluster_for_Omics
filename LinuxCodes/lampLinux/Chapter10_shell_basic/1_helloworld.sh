8e00cd975b66cea3a0e434c56807d4434804de2a
WSL_Oracle <bioinfo.gao@gmail.com>
2025-11-19 00:01:49 -0500

    keyboard shortcuts

---
+++ LinuxCodes/lampLinux/Chapter10_shell_basic/1_helloworld.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+# Auther ZG
+# 2 method to run : 
+# bash 1_helloworld.sh
+# chmod 0755 1_helloworld.sh
+# ./1_bash 1_helloworld.sh
+
+echo "hello world!"
+
---
+++ LinuxCodes/lampLinux/Chapter10_shell_basic/3_fast_key.md
@@ -0,0 +1,6 @@
+Ctrl + C # stop 
+Ctrl + L # clear
+Ctrl + U # delete all earlier characteristics
+Ctrl + Y # paste 
+Ctrl + D # logout
+Ctrl + Z # bg work
