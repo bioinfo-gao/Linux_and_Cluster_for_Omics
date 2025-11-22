f510b85327ea7b4432b7b05b4b0c7e1e13f3a76d
WSL_Oracle <bioinfo.gao@gmail.com>
2025-11-22 12:58:05 -0500

    printf

--- LinuxCodes/lampLinux/Chapter11_shell_programming/2_column_extract/cut.sh
+++
@@ -1,9 +0,0 @@
-
-# cat ../data/student1.txt # 分割符必须是tab ， 不能是空格
-# cut -f 2 ../data/student1.txt 
-# cut -f 3 ../data/student1.txt 
-# cut -f 4  ../data/student1.txt 
-# cut -f 2,3  ../data/student1.txt 
-# cut -d " " -f 2 ../data/student1.txt 
-cut -d ":" -f 1,3 /etc/passwd 
-
