5003f7c5bb990540c923e3a9b96ff1bca44a0069
WSL_Oracle <bioinfo.gao@gmail.com>
2025-11-30 23:47:39 -0500

    grub crypt

--- Linux_Code_LampBrother/Chapter15_startup_and_reboot/2_grub.sh
+++ Linux_Code_LampBrother/Chapter15_startup_and_reboot/2_grub.sh
@@ -29,6 +29,31 @@ title Linux  # 启动菜单项名称
 装多系统是，应该先装Windows，再装Linux，因为Windows安装程序会覆盖MBR，引导Linux启动项会丢失
 linux 可以识别 Windows 引导项，并将其添加到 grub 启动菜单中
 
+生产服务器基本上不会装多系统，因为多系统会增加维护复杂度和安全风险
+不管是Windows 还是Linux，生产服务器一般只装一个系统，确保系统稳定和安全运行
 
 # (3) ============= 更新 grub 配置文件 ========================
 update-grub   # 生成或更新 grub 配置文件 /boot/grub/grub
+
+# (3) =============  grub 加密 ========================
+grub 引导程序    可以通过设置密码来保护引导菜单，防止未经授权的用户修改启动选项或进入单用户模式
+grub-mkpasswd-pbkdf2   # 生成 grub 密码散列值
+# 输入密码后，生成类似如下的散列值  
+# PBKDF2 hash of your password is grub.pbkdf2.sha512.10000.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+# 在 /etc/grub.d/40_custom 文件中添加如下内容，设置 grub 密码保护
+set superusers="root"
+password_pbkdf2 root <generated_hash_value>
+
+
+
+grub-md5-crypt   # 生成 grub md5 加密密码, 
+grub-mkpasswd-pbkdf2 # SHA512 更安全，推荐使用 
+# 手工输入密码后，生成类似如下的 md5 散列值
+# $1$XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+# 在 /etc/grub.d/40_custom 文件中添加如下内容，设置 grub 密码保护
+set superusers="root"
+password_md5 root <generated_md5_hash_value> # 注意这里是 md5， 不是 pbkdf2
+# 然后更新 grub 配置文件
+update-grub
