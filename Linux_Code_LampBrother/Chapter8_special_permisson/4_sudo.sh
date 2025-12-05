15bdb6497802e5cff102d39e5159719d9e447267
WSL_Oracle <bioinfo.gao@gmail.com>
2025-12-04 23:43:50 -0500

    sudo -E

--- Linux_Code_LampBrother/Chapter8_special_permisson/4_sudo.sh
+++ Linux_Code_LampBrother/Chapter8_special_permisson/4_sudo.sh
@@ -20,3 +20,11 @@ visudo # visudo:  just == vim /etc/sudoers: Permission denied
 gao ALL=/sbin/shutdown -r now
 sc  192.168.0.156=/usr/bin/vim # very Dangerour!  不要赋予这样的工具 管理员权限
 # IP 被管理的IP
+
+
+# sudo 的时候，原用户的各种配置均无法使用
+Use the -E flag to preserve your user's environment variables, so sudo inherits your $HOME and loads your configs.
+
+Example for editing a file with Vim:text
+
+udo -E vim /path/to/file # -E
