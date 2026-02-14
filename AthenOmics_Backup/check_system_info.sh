bbeea306cb32eea6fca82faf28bc115ce7e2373c
gaoz from AthenOmics <bioinfo.gao@gmail.com>
2026-02-15 01:08:20 +0800

    CPU and Memory

--- AthenOmics_Backup/check_system_info.sh
+++ AthenOmics_Backup/check_system_info.sh
@@ -3,6 +3,10 @@
 
 cat /etc/os-release # 检查 Linux 发行版（Distro）最标准且信息最全的方法
 uname -a # 显示系统内核版本和其他相关信息
-
+hostnamectl # 显示主机名和系统信息, Best for checking the Linux distribution and version
+lsb_release -a # 显示 Linux 发行版信息, 需要安装 lsb-release 包
+cat /proc/cpuinfo # 显示 CPU 信息, 包括型号、核心数、线程数等，但信息过长无法检查
+cat /proc/cpuinfo | grep processor # 显示 CPU 信息，0-55 共 56 个处理器核心
+cat /proc/meminfo # 显示内存信息
 # Ubuntu 授予 sudo
 usermod -aG sudo gaoz
